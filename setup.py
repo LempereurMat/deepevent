501222701837a71900acfa95b4827ca2533ff431
LEMPEREUR Mathieu <mathieu.lempereur@univ-brest.fr>
2019-10-04 17:17:07 +0200

    Update

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from setuptools import find_packages,setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="deepevent",
+    version="0.1",
+    author="Lempereur Mathieu",
+    author_email="mathieu.lempereur@univ-brest.fr",
+    description="Deep Learning to identify gait events",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    include_package_data=True,
+    packages=find_packages(),
+    scripts=['deepevent/deepevent.py']
+    
+)
