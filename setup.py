4fee3a367a64600017cb7f57f6a7f7efa881b6a0
LEMPEREUR Mathieu <mathieu.lempereur@univ-brest.fr>
2020-03-31 11:18:56 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf8 -*-
+
 from setuptools import find_packages,setup
 
 with open("README.md", "r") as fh:
@@ -6,20 +8,22 @@ with open("README.md", "r") as fh:
 
 setup(
     name="deepevent",
-    version="0.3.3",
+    version="1.0",
     author="Lempereur Mathieu",
     author_email="mathieu.lempereur@univ-brest.fr",
     description="Deep Learning to identify gait events",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    include_package_data=True,
     packages=find_packages(),
-    scripts=['deepevent/deepevent_script.py'],
-    install_requires=['tensorflow>=2.0.0,<2.1.0',
+	package_data={'deepevent': ['data/DeepEvent*.*']},
+	#scripts=['deepevent\deepevent.py'],
+    entry_points={'console_scripts': ['deepevent = deepevent.deepevent:main']},
+    install_requires=['tensorflow>=2.1.0',
                       'keras>=2.3.1',
                       'numpy>=1.18.1',
                       'scipy>=1.4.1',
-                      'pyBTK>=0.1.1'],
+                      'pyBTK>=0.1.1',
+					  'googledrivedownloader==0.4'],
     classifiers=['Programming Language :: Python',
                  'Programming Language :: Python :: 3.7',
                  'Operating System :: Microsoft :: Windows',
